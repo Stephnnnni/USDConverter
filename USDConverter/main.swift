2414a43e60562fefdb3b226e59aaf78c3a0ee21e
Peter Wunder <hallo@peterwunder.de>
2019-06-11 12:58:28 +0200

    Initial Commit

---
+++ USDConverter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  USDConverter
+//
+//  Created by Peter Wunder on 11.06.19.
+//  Copyright © 2019 Peter Wunder. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
